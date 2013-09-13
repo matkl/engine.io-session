6ed5439cbdc92e2643487e67cde9f0fc9f7f3b4e
Matthias Klein <mat@jumpcat.de>
2013-09-13 04:48:17 +0200

    Add test

---
+++ test/session.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var connect = require('connect');
+var eio = require('engine.io');
+var eioc = require('engine.io-client');
+var eioSession = require('..');
+var superagent = require('superagent');
+
+function listen(callback) {
+  var cookieParser = connect.cookieParser();
+  var app = connect();
+  var sessionStore = new connect.session.MemoryStore();
+  var sessionKey = 'sid';
+  var sessionSecret = 'keyboard cat';
+
+  app.use(connect.cookieParser());
+  app.use(connect.session({ store: sessionStore, key: sessionKey, secret: sessionSecret }));
+  app.use(function(req, res) {
+    req.session.foo = 'bar';
+    req.session.save();
+    res.end('test');
+  });
+
+  var httpServer = http.createServer(app).listen();
+  var server = eio.attach(httpServer);
+
+  server.on('connection', eioSession({
+    cookieParser: cookieParser,
+    store: sessionStore,
+    key: sessionKey,
+    secret: sessionSecret
+  }));
+
+  callback(httpServer.address().port);
+
+  return server;  
+}
+
+function request(port, callback) {
+  var agent = superagent.agent();
+  agent.get('http://localhost:' + port).end(function(err, res) {
+    process.nextTick(function() {
+      callback(agent);
+    });
+  });
+}
+
+function HttpAgent(agent) {
+  this.agent = agent;
+}
+
+HttpAgent.prototype.addRequest = function(req, host, port, localAddress) {
+  var url = 'http://' + host + ':' + port + req.path;
+  this.agent.get(url).end();
+};
+
+describe('engine.io-session', function() {
+  it('should get a session', function(done) {
+    var server = listen(function(port) {
+      request(port, function(agent) {
+        var socket = eioc('ws://localhost:' + port, { agent: new HttpAgent(agent) });
+      });
+    });
+
+    server.on('session', function(socket, session) {
+      done();
+    });
+  });
+});
