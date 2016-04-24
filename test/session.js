0828f12dc05a8f85feefb486036086f64cfc0da6
Matthias Klein <matthias@klein.pw>
2016-04-25 00:53:24 +0200

    update dependencies, add coverage report

--- test/session.js
+++ test/session.js
@@ -35,29 +35,13 @@ function listen(callback) {
   return server;  
 }
 
-function request(port, callback) {
-  var agent = superagent.agent();
-  agent.get('http://localhost:' + port).end(function(err, res) {
-    process.nextTick(function() {
-      callback(agent);
-    });
-  });
-}
-
-function HttpAgent(agent) {
-  this.agent = agent;
-}
-
-HttpAgent.prototype.addRequest = function(req, host, port, localAddress) {
-  var url = 'http://' + host + ':' + port + req.path;
-  this.agent.get(url).end();
-};
-
 describe('engine.io-session', function() {
   it('should get a session', function(done) {
     var server = listen(function(port) {
-      request(port, function(agent) {
-        var socket = eioc('ws://localhost:' + port, { agent: new HttpAgent(agent) });
+      var agent = superagent.agent();
+      
+      agent.get('http://localhost:' + port).end(function(err, res) {
+        var socket = eioc('ws://localhost:' + port, { agent: res.agent });
       });
     });
 
@@ -65,6 +49,7 @@ describe('engine.io-session', function() {
       done();
     });
   });
+
   it('should generate a session if none exists', function(done) {
     var server = listen(function(port) {
       var socket = eioc('ws://localhost:' + port);
